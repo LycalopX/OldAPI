037041f83402456896d585a45a01554fee08eae1
Alexandre Weber <84647460+LycalopX@users.noreply.github.com>
2024-06-24 14:50:49 -0300

    Ok, agora não tem mais aquela página zoando os ido sos

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,13 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pages: {
-    index: {
-      entry: 'src/pages/index/main.js',
-      template: 'public/index.html',
-      filename: 'index.html',
-      title: 'Home',
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
-    },
     teste: {
       entry: 'src/pages/teste/main.js',
       template: 'public/index.html',
